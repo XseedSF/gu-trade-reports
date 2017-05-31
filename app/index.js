fa0bf3d15e7f6bc07ba79a66fa4c54b2fe4135a5
Pablo Corso <pcorso@xseed.com.uy>
2017-05-31 16:24:37 -0300

    Add base webpack.config and dependencies.

--- app/index.js
+++ app/index.js
@@ -1,4 +1,36 @@
-// exports.Books = require("./reactTest.jsx");
-// exports.App = require("./app.js");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
 
-// export { default as App } from './app.js';
+import configureStore from './configureStore'
+import { fetchForm, toggleAnswerFilter, clearFilters } from './actions';
+import { questionsFiltersSelector } from './selectors';
+
+const initialData = document.getElementById('initial-data');
+const form = JSON.parse(initialData.text);
+
+
+// <App form={form}/>,
+
+var store = configureStore();
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
+store.dispatch(fetchForm(form));
+
+store.subscribe(function () {
+	const state = store.getState();
+	if (state.form && state.form.result > 0) {
+		const filters = questionsFiltersSelector(state);
+		console.log(filters);
+	}
+});
+// var questionFilters = questionsFiltersSelector(store.getState());
+
+initialData.remove();
