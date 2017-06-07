71e7b95c633a1fd3c34c7553e7dc50738da95efd
Pablo Corso <pcorso@xseed.com.uy>
2017-06-07 12:05:47 -0300

    Avoid title children array warning.

--- app/components/Title.js
+++ app/components/Title.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Title = ({ children }) => (
 	<div className='nav_menu'>
@@ -8,9 +7,5 @@ const Title = ({ children }) => (
 		</div>
 	</div>
 )
-
-Title.propTypes = {
-	children: PropTypes.element.isRequired,
-}
 
 export default Title;
