ce4b35608ff75b0cc83816ebea5dcc407d83747a
PabloCorso <pabcorso@gmail.com>
2017-06-09 16:59:57 -0300

    Use recompose for app lifecycle.

--- app/components/App.js
+++ app/components/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom';
+import React from 'react'
 import Header from './Header';
 import Filters from './Filters';
 import { AppContainer } from '../containers';
+import { compose, lifecycle } from 'recompose';
 import '../App.css';
 import '../fixed-data-table.css';
 
-class App extends Component {
-	componentDidMount() {
-		this.fetchData();
-	}
+const App = () =>
+	<div className='App'>
+		<Header />
+		<Filters />
+	</div>;
 
-	fetchData() {
+const withFetchForm = lifecycle({
+	componentDidMount() {
 		const filtersQuery = this.props.location.search;
 		this.props.fetchForm(filtersQuery);
 	}
+});
 
-	render() {
-		return (
-			<div className='App'>
-				<Header />
-				<Filters />
-			</div>
-		);
-	}
-}
-
-export default AppContainer(App);
+export default compose(
+	AppContainer,
+	withFetchForm,
+)(App);
