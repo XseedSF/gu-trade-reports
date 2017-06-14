49c3d275853ae37de5e03d94d129788c1fd24b16
PabloCorso <pabcorso@gmail.com>
2017-06-14 14:28:31 -0300

    Remove react static table dependency.

--- app/components/FormAnswers.js
+++ app/components/FormAnswers.js
@@ -4,44 +4,30 @@ import FormAnswer from './FormAnswer';
 import NoDataMatching from './NoMatchingData';
 import container from '../containers/FormAnswers';
 import { compose, setPropTypes } from 'recompose';
-import { Table, Column, Cell } from 'fixed-data-table';
 import withSpinnerWhileLoading from '../hocs/withSpinnerWhileLoading';
 
 const FormAnswers = ({ questions, completedForms }) =>
-	<div className='table-centered'>
-		<Table
-			rowsCount={completedForms.length}
-			rowHeight={50}
-			headerHeight={50}
-			width={1000}
-			maxHeight={500}>
-			<Column
-				flexGrow={1}
-				width={200}
-				header={<Cell>Nombre del punto de venta</Cell>}
-				cell={props => (
-					<Cell {...props}>
-						{completedForms[props.rowIndex].name}
-					</Cell>
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<th>Nombre del punto de venta</th>
+					{questions.map(question => <th key={question.Id}>{question.Text}</th>)}
+				</tr>
+			</thead>
+			<tbody>
+				{completedForms.map(completedForm =>
+					<tr key={completedForm.Id}>
+						<td>{completedForm.name}</td>
+						{completedForm.answers.map(answer =>
+							<td key={answer.Id}>
+								<FormAnswer answer={answer} />
+							</td>
+						)}
+					</tr>
 				)}
-			/>
-			{questions.map(question =>
-				<Column key={question.Id}
-					flexGrow={1}
-					width={200}
-					header={<Cell>{question.Text}</Cell>}
-					cell={props => (
-						<Cell {...props}>
-							{completedForms[props.rowIndex].answers
-								.filter(answer => answer.QuestionId === question.Id)
-								.map(answer =>
-									<FormAnswer key={answer.Id} answer={answer} />
-								)}
-						</Cell>
-					)}
-				/>
-			)}
-		</Table>
+			</tbody>
+		</table>
 		{completedForms && completedForms.length === 0 && <NoDataMatching />}
 	</div>;
 
