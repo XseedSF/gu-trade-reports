06e040900c7e080a3b0fde570cb5912df1fd4865
PabloCorso <pabcorso@gmail.com>
2017-06-14 09:09:39 -0300

    Fix unselect filter.

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,27 +1,7 @@
-export const filterForms = (forms, filters, chartQuestion) => {
-	//Chart Question es opcional
-	let filteredForms = forms.filter(f => {
-		let addForm = true;
-		f.questions.forEach(function (question) {
-			if (!chartQuestion || question.Id !== chartQuestion.Id) {
-				filters.forEach(function (filter) {
-					if (addForm && question.Id === filter.questionId) {
-						switch (question.Type) {
-							case "MO":
-								addForm = filter.values.indexOf(question.SelectedOptionId) !== -1;
-								break;
-							case "YN":
-								addForm = filter.values.indexOf(question.YesNoValue) !== -1;
-								break;
-							default:
-								break;
-						}
-					}
-				});
-			}
-		});
-		return addForm;
-	});
-
-	return filteredForms;
+export const deleteAttribute = (key) => {
+	return (state) => {
+		const newState = Object.assign({}, state);
+		delete newState[key];
+		return newState;
+	}
 };
--- app/reducers/filters.js
+++ app/reducers/filters.js
@@ -1,42 +1,46 @@
 import { actions, filterTypes } from '../constants';
+import { deleteAttribute } from '../lib/utils';
 
 const filter = (state = {}, action) => {
-  switch (action.type) {
-    case actions.TOGGLE_ANSWER_FILTER:
-      const { id, type, value } = action.payload;
-      const { selected = [] } = state;
+	switch (action.type) {
+		case actions.TOGGLE_ANSWER_FILTER:
+			const { id, type, value } = action.payload;
+			const { selected = [] } = state;
 
-      const alreadySelected = selected.includes(value);
+			const alreadySelected = selected.includes(value);
 
-      switch (type) {
-        case filterTypes.SINGLE_SELECT:
-          return { id, selected: alreadySelected ? selected.filter((e) => e != value) : [value] };
-        case filterTypes.MULTI_SELECT:
-          return { id, selected: alreadySelected ? selected.filter((e) => e != value) : [...selected, value] };
-        default:
-          return state;
-      }
-    default:
-      return state;
-  }
+			switch (type) {
+				case filterTypes.SINGLE_SELECT:
+					return { id, selected: alreadySelected ? selected.filter((e) => e != value) : [value] };
+				case filterTypes.MULTI_SELECT:
+					return { id, selected: alreadySelected ? selected.filter((e) => e != value) : [...selected, value] };
+				default:
+					return state;
+			}
+		default:
+			return state;
+	}
 };
 
 const filters = (state = {}, action) => {
-  switch (action.type) {
-    case actions.TOGGLE_ANSWER_FILTER:
-      const { id } = action.payload;
-      const existingFilter = state[id];
-      const newFilter = filter(existingFilter, action);
+	switch (action.type) {
+		case actions.TOGGLE_ANSWER_FILTER:
+			const { id } = action.payload;
+			const existingFilter = state[id];
+			const newFilter = filter(existingFilter, action);
 
-      const shouldRemove = newFilter.selected ? newFilter.selected.length === 0 : false;
-
-      if (shouldRemove) return (({ [id]: deleted, ...state }) => state)(state);
-      else return { ...state, [id]: newFilter };
-    case actions.CLEAR_FILTERS:
-      return {};
-    default:
-      return state;
-  }
+			const shouldRemove = newFilter.selected ? newFilter.selected.length === 0 : false;
+			if (shouldRemove) {
+				const deleteFilter = deleteAttribute(id);
+				return deleteFilter(state);
+			} else {
+				return { ...state, [id]: newFilter }
+			};
+		case actions.CLEAR_FILTERS:
+			return {};
+		default:
+			return state;
+	}
 };
 
 export default filters;
