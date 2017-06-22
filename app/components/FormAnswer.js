f0b32aad54f9b62ec3f05ccbd56987b86ba9d5c5
PabloCorso <pabcorso@gmail.com>
2017-06-22 18:02:06 -0300

    Add checkbox answer display for the table.

--- app/components/FormAnswer.js
+++ app/components/FormAnswer.js
@@ -1,22 +1,27 @@
 import React from "react";
 import ImageAnswer from "./ImageAnswer";
 import DateText from "./DateText";
+import { questionTypes } from "../constants";
 
 const FormAnswer = ({ answer, name }) => {
   const renderReponse = answer => {
     switch (answer.Type) {
-      case "MO":
+      case questionTypes.MULTIPLE_OPTION:
         return <span>{answer.SelectedOptionName}</span>;
-      case "YN":
+      case questionTypes.YES_NO:
         return <span>{answer.YesNoValue ? "Sí" : "No"}</span>;
-      case "FT":
+      case questionTypes.FREE_TEXT:
         return <span> {answer.FreeText} </span>;
-      case "CAM":
+      case questionTypes.CAMERA:
         return answer.ImageBase64
           ? <ImageAnswer image={answer.ImageBase64} title={name} />
           : <span> Sin imagen </span>;
-      case "DATE":
+      case questionTypes.DATE:
         return <span>{<DateText value={answer.value} />}</span>;
+      case questionTypes.CHECKBOX:
+        return answer.value
+          ? <input type="checkbox" disabled="disabled" checked />
+          : <input type="checkbox" disabled="disabled" />;
       default:
         return <span> </span>;
     }
