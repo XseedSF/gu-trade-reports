4ba4d821be024d063915112d8d52267410ab15a5
PabloCorso <pabcorso@gmail.com>
2017-07-31 15:24:57 -0300

    Format form report data access.

--- server/controllers/baseController.js
+++ server/controllers/baseController.js
@@ -1,21 +1,21 @@
 var path = require('path');
 
 class BaseController {
-  constructor(app, controllerName) {
-    this.app = app;
-    this.viewspath = path.dirname(require.main.filename) + "/views/";
-    //this.controllerFolder = controllerName;
-  }
+	constructor(app, controllerName) {
+		this.app = app;
+		this.viewspath = path.dirname(require.main.filename) + "/views/";
+		//this.controllerFolder = controllerName;
+	}
 
-  /// Get - Set
-  getApp() {
-    return this.app;
-  }
+	/// Get - Set
+	getApp() {
+		return this.app;
+	}
 
-  /// Controller Logic
-  returnView(res, view) {
-    res.render(view);
-  }
+	/// Controller Logic
+	returnView(res, view) {
+		res.render(view);
+	}
 }
 
 module.exports = BaseController;
