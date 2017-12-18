505a7c51fa50f7ee4f8104f404df68c2f7d1dcb8
PabloCorso <pabcorso@gmail.com>
2017-12-18 10:44:43 -0300

    Fix null dateReply at Table.

--- app/components/DateText.js
+++ app/components/DateText.js
@@ -2,6 +2,6 @@ import React from "react";
 import moment from "moment";
 
 const DateText = ({ value }) =>
-  value != null ? <span>{moment(value).format("DD/MM/YYYY")}</span> : "";
+	value ? <span>{moment(value).format("DD/MM/YYYY")}</span> : null;
 
 export default DateText;
--- server/guTradeApi/formApi.js
+++ server/guTradeApi/formApi.js
@@ -4,151 +4,151 @@ const httpRequest = require("./httpRequest");
 const questionTypes = require("../constants").questionTypes;
 
 class FormApi extends BaseApi {
-  constructor(envCode) {
-    super(envCode);
-  }
+	constructor(envCode) {
+		super(envCode);
+	}
 
-  getCompletedForms(filter, success, error) {
-    this.login()
-      .then(sessionToken => {
-        httpRequest.setAuthentication(sessionToken);
+	getCompletedForms(filter, success, error) {
+		this.login()
+			.then(sessionToken => {
+				httpRequest.setAuthentication(sessionToken);
 
-        const formRequest = {
-          action: "form/GetForm",
-          data: {
-            formId: filter.formId
-          }
-        };
-        httpRequest
-          .get(formRequest)
-          .then(formResponse => {
-            const form = getFormFromResponse(formResponse);
+				const formRequest = {
+					action: "form/GetForm",
+					data: {
+						formId: filter.formId
+					}
+				};
+				httpRequest
+					.get(formRequest)
+					.then(formResponse => {
+						const form = getFormFromResponse(formResponse);
 
-            const completedFormsRequest = {
-              action: "form/GetCompletedForms",
-              data: filter
-            };
-            httpRequest
-              .get(completedFormsRequest)
-              .then(completedFormsResponse => {
-                const completedForms = getCompletedFormsFromResponse(
-                  completedFormsResponse
-                );
+						const completedFormsRequest = {
+							action: "form/GetCompletedForms",
+							data: filter
+						};
+						httpRequest
+							.get(completedFormsRequest)
+							.then(completedFormsResponse => {
+								const completedForms = getCompletedFormsFromResponse(
+									completedFormsResponse
+								);
 
-                form.completedForms = completedForms;
-                success(form);
-              })
-              .catch(error);
-          })
-          .catch(error);
-      })
-      .catch(error);
-  }
+								form.completedForms = completedForms;
+								success(form);
+							})
+							.catch(error);
+					})
+					.catch(error);
+			})
+			.catch(error);
+	}
 }
 
-const getFormFromResponse = function({ data: response }) {
-  const formResponse = response.Data;
-  const form = {
-    id: formResponse.Id,
-    name: formResponse.Name,
-    questions: [],
-    completedForms: []
-  };
+const getFormFromResponse = function ({ data: response }) {
+	const formResponse = response.Data;
+	const form = {
+		id: formResponse.Id,
+		name: formResponse.Name,
+		questions: [],
+		completedForms: []
+	};
 
-  for (let i = 0; i < formResponse.Questions.length; i++) {
-    const questionResponse = formResponse.Questions[i];
-    question = {
-      Id: questionResponse.Id,
-      Type: questionResponse.Type.Code,
-      Text: questionResponse.Text,
-      Required: questionResponse.Required
-    };
+	for (let i = 0; i < formResponse.Questions.length; i++) {
+		const questionResponse = formResponse.Questions[i];
+		question = {
+			Id: questionResponse.Id,
+			Type: questionResponse.Type.Code,
+			Text: questionResponse.Text,
+			Required: questionResponse.Required
+		};
 
-    if (questionResponse.Options.length > 0) {
-      question.Options = [];
-      for (let j = 0; j < questionResponse.Options.length; j++) {
-        const option = questionResponse.Options[j];
-        question.Options.push({
-          Id: option.Id,
-          Text: option.Text
-        });
-      }
-    }
+		if (questionResponse.Options.length > 0) {
+			question.Options = [];
+			for (let j = 0; j < questionResponse.Options.length; j++) {
+				const option = questionResponse.Options[j];
+				question.Options.push({
+					Id: option.Id,
+					Text: option.Text
+				});
+			}
+		}
 
-    form.questions.push(question);
-  }
+		form.questions.push(question);
+	}
 
-  return form;
+	return form;
 };
 
-const getCompletedFormsFromResponse = function({ data: response }) {
-  const completedFormsResponse = response.Data;
-  const completedForms = [];
-  let cfReponse = null,
-    completedForm = null;
-  for (let i = 0; i < completedFormsResponse.length; i++) {
-    cfReponse = completedFormsResponse[i];
-    const completedForm = {
-      Id: cfReponse.Id,
-      name: cfReponse.PointOfInterestName,
-      pointOfInterestId: cfReponse.PointOfInterestId,
-      answers: []
-    };
+const getCompletedFormsFromResponse = function ({ data: response }) {
+	const completedFormsResponse = response.Data;
+	const completedForms = [];
+	let cfReponse = null,
+		completedForm = null;
+	for (let i = 0; i < completedFormsResponse.length; i++) {
+		cfReponse = completedFormsResponse[i];
+		const completedForm = {
+			Id: cfReponse.Id,
+			name: cfReponse.PointOfInterestName,
+			pointOfInterestId: cfReponse.PointOfInterestId,
+			answers: []
+		};
 
-    for (let j = 0; j < cfReponse.Answers.length; j++) {
-      const cfAnswer = cfReponse.Answers[j];
+		for (let j = 0; j < cfReponse.Answers.length; j++) {
+			const cfAnswer = cfReponse.Answers[j];
 
-      const isMultipleOption =
-        cfAnswer.Question.Type.Code === questionTypes.MULTIPLE_OPTION;
-      const answer = {
-        Id: cfAnswer.Id,
-        QuestionId: cfAnswer.Question.Id,
-        Type: cfAnswer.Question.Type.Code,
-        Text: cfAnswer.Question.Text,
-        YesNoValue: cfAnswer.YesNoOption,
-        FreeText: cfAnswer.FreeText,
-        SelectedOptionId: isMultipleOption
-          ? cfAnswer.ChosenQuestionOption.Id
-          : null,
-        SelectedOptionName: isMultipleOption
-          ? cfAnswer.ChosenQuestionOption.Text
-          : null,
-        ImageBase64: cfAnswer.ImageArray,
-        DateReply: cfAnswer.DateReply,
-        value: getAnswerValue(cfAnswer)
-      };
-      completedForm.answers.push(answer);
-    }
+			const isMultipleOption =
+				cfAnswer.Question.Type.Code === questionTypes.MULTIPLE_OPTION;
+			const answer = {
+				Id: cfAnswer.Id,
+				QuestionId: cfAnswer.Question.Id,
+				Type: cfAnswer.Question.Type.Code,
+				Text: cfAnswer.Question.Text,
+				YesNoValue: cfAnswer.YesNoOption,
+				FreeText: cfAnswer.FreeText,
+				SelectedOptionId: isMultipleOption
+					? cfAnswer.ChosenQuestionOption.Id
+					: null,
+				SelectedOptionName: isMultipleOption
+					? cfAnswer.ChosenQuestionOption.Text
+					: null,
+				ImageBase64: cfAnswer.ImageArray,
+				DateReply: cfAnswer.DateReply,
+				value: getAnswerValue(cfAnswer)
+			};
+			completedForm.answers.push(answer);
+		}
 
-    completedForms.push(completedForm);
-  }
+		completedForms.push(completedForm);
+	}
 
-  return completedForms;
+	return completedForms;
 };
 
 const getAnswerValue = answer => {
-  switch (answer.Question.Type.Code) {
-    case questionTypes.CHECKBOX:
-      return answer.CheckOption;
-    case questionTypes.YES_NO:
-      return answer.YesNoOption;
-    case questionTypes.MULTIPLE_OPTION:
-      return answer.ChosenQuestionOption.Id;
-    case questionTypes.CAMERA:
-      return !answer.Skipped && answer.ImageArray != null;
-    case questionTypes.DATE:
-      return !answer.Skipped && answer.DateReply != null
-        ? answer.DateReply.getTime() + 3 * 60 * 60 * 1000
-        : null;
-    case questionTypes.NUMERIC:
-      const value = parseFloat(answer.FreeText);
-      return !answer.Skipped && value ? value : 0;
-    case questionTypes.FREE_TEXT:
-    case questionTypes.BAR_CODE:
-    case questionTypes.SIGNATURE:
-    case questionTypes.IMAGE:
-      return !answer.Skipped;
-  }
+	switch (answer.Question.Type.Code) {
+		case questionTypes.CHECKBOX:
+			return answer.CheckOption;
+		case questionTypes.YES_NO:
+			return answer.YesNoOption;
+		case questionTypes.MULTIPLE_OPTION:
+			return answer.ChosenQuestionOption.Id;
+		case questionTypes.CAMERA:
+			return !answer.Skipped && answer.ImageArray != null;
+		case questionTypes.DATE:
+			return !answer.Skipped && answer.DateReply != null
+				? new Date(answer.DateReply).getTime() + 3 * 60 * 60 * 1000
+				: "";
+		case questionTypes.NUMERIC:
+			const value = parseFloat(answer.FreeText);
+			return !answer.Skipped && value ? value : 0;
+		case questionTypes.FREE_TEXT:
+		case questionTypes.BAR_CODE:
+		case questionTypes.SIGNATURE:
+		case questionTypes.IMAGE:
+			return !answer.Skipped;
+	}
 };
 
 module.exports = FormApi;
