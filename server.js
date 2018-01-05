ea8971826db610c6b6ef31ee603cb6b3358b3066
PabloCorso <pabcorso@gmail.com>
2018-01-05 14:04:59 -0300

    Add pm2 config.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const webpack = require("webpack");
